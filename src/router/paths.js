1208b0b51f3d80ebad49f18c80fa6fbda068cb61
mo7mad4 <mohammedreda814@gmail.com>
2023-07-15 20:47:39 +0300

    Create router at this project

---
+++ src/router/paths.js
@@ -0,0 +1,15 @@
+export const PATHS = {
+    HOME: '/',
+    ABOUT: '/about',
+    
+    STORES: {
+      ROOT: '/stores',
+      VIEW: `/stores/:id`,
+      CREATE: '/stores/create',
+      EDIT: '/stores/:id/edit',
+    },
+  
+    ERRORS: {
+      NOT_FOUND: '404',
+    },
+  };
