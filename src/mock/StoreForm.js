5b556e3acd7019ee32dcc17431113945373c790f
mo7mad4 <mohammedreda814@gmail.com>
2023-07-17 23:06:05 +0300

    Make mock data for Store form

---
+++ src/mock/StoreForm.js
@@ -0,0 +1,15 @@
+const inputsArray = [
+    {
+      id: "name",
+      name: "name",
+      type: "text",
+      label: "Name",
+    },
+    {
+      id: "cities",
+      name: "cities",
+      type: "textarea",
+      label: "Cities",
+    },
+  ];
+export default inputsArray;  
